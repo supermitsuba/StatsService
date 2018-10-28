be6020b60fd5fc9cea9a70ca992c8a4a6614431e
Jorden Lowe <supermitsuba@users.noreply.github.com>
2018-10-28 18:48:23 -0400

    Update createTable.sql
    DateTime saves far beyond 2038

--- createTable.sql
+++ createTable.sql
@@ -4,6 +4,6 @@ CREATE TABLE stats (
     id              BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
     value           varchar(50), 
     type            varchar(50),
-    dateOfOccurance TIMESTAMP, 
+    dateOfOccurance DATETIME, 
     nameOfMachine   varchar(50)
 );
