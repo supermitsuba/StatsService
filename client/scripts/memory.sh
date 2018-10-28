a0992cc6806e40795534e30ffe69403a8ffaa8dc
Jorden Lowe <supermitsuba@users.noreply.github.com>
2018-10-28 19:42:44 -0400

    Create memory.sh

---
+++ client/scripts/memory.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Name 
+nameOfMachine=""
+
+# url
+url=""
+
+# values
+myTimeStamp=$(date +"%Y-%m-%dT%H:%M:%S")
+jsonTemplate='{ "type":"%s", "value":"%s", "dateOfOccurance":"%s", "nameOfMachine":"%s" }'
+
+memFree="$(grep MemFree /proc/meminfo | awk {'print $2'}) kB"
+memFreeData=$(printf "$jsonTemplate" "memFree" "$memFree" "$myTimeStamp" "$nameOfMachine")
+curl --request POST \
+     --header "Content-Type: application/json" \
+     --data "$memFreeData" \
+     $url
+
+memTotal="$(grep MemTotal /proc/meminfo | awk {'print $2'}) kB"
+memTotalData=$(printf "$jsonTemplate" "memTotal" "$memTotal" "$myTimeStamp" "$nameOfMachine")
+curl --request POST \
+     --header "Content-Type: application/json" \
+     --data "$memTotalData" \
+     $url
