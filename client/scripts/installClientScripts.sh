9eadcbfc66321ced140db2f47c99db489dc8401e
Jorden Lowe <supermitsuba@users.noreply.github.com>
2018-10-28 21:23:46 -0400

    Create installClientScripts.sh

---
+++ client/scripts/installClientScripts.sh
@@ -0,0 +1,10 @@
+dir="/usr/src/statsScripts"
+
+mkdir -p $dir
+wget -O $dir/cpu.sh https://raw.githubusercontent.com/supermitsuba/StatsService/master/client/scripts/cpu.sh
+wget -O $dir/ip.sh https://raw.githubusercontent.com/supermitsuba/StatsService/master/client/scripts/ip.sh 
+wget -O $dir/linux.sh https://raw.githubusercontent.com/supermitsuba/StatsService/master/client/scripts/linux.sh 
+wget -O $dir/memory.sh https://raw.githubusercontent.com/supermitsuba/StatsService/master/client/scripts/memory.sh 
+wget -O $dir/temperature.sh https://raw.githubusercontent.com/supermitsuba/StatsService/master/client/scripts/temperature.sh 
+wget -O $dir/uptime.sh https://raw.githubusercontent.com/supermitsuba/StatsService/master/client/scripts/uptime.sh 
+chmod +x $dir/*.sh
