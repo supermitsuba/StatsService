48aa738b19650b347294a027e8bebace3255563f
Jorden Lowe <supermitsuba@users.noreply.github.com>
2018-10-28 20:56:35 -0400

    Update crontab.sh

--- client/scripts/crontab.sh
+++ client/scripts/crontab.sh
@@ -1,7 +1,7 @@
-0 * * * * /usr/src/statsScripts/cpu.sh
-@reboot /usr/src/statsScripts/ip.sh
-@reboot /usr/src/statsScripts/linuxName.sh
-0 * * * * /usr/src/statsScripts/memory.sh
-0 */10 * * * /usr/src/statsScripts/process.sh
-0 0 * * * /usr/src/statsScripts/uptime.sh
-0 * * * * /usr/src/statsScripts/temperature.sh
+* * * * * /usr/src/statsScripts/cpu.sh &
+@reboot /usr/src/statsScripts/ip.sh &
+@reboot /usr/src/statsScripts/linuxName.sh &
+* * * * * /usr/src/statsScripts/memory.sh &
+*/10 * * * * /usr/src/statsScripts/process.sh &
+0 * * * * /usr/src/statsScripts/uptime.sh &
+* * * * * /usr/src/statsScripts/temperature.sh
