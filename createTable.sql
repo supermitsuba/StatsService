fae4929aaee7b0ccaa4f942f6b064a357cd6837f
Jorden Lowe <supermitsuba@users.noreply.github.com>
2018-10-30 10:55:44 -0400

    Update createTable.sql

--- createTable.sql
+++ createTable.sql
@@ -1,9 +1,12 @@
 use iot;
 
-CREATE TABLE stats (
-    id              BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-    value           varchar(50), 
-    type            varchar(50),
-    dateOfOccurance DATETIME, 
-    nameOfMachine   varchar(50)
-);
+CREATE TABLE `stats` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `value` varchar(50) DEFAULT NULL,
+  `type` varchar(50) DEFAULT NULL,
+  `dateOfOccurance` datetime DEFAULT NULL,
+  `nameOfMachine` varchar(50) DEFAULT NULL,
+  `createdTimestamp` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=latin1;
+
