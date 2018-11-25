2ade46a03fd1af2bd82245f93980d18f43714a5c
Jorden Lowe <supermitsuba@users.noreply.github.com>
2018-11-25 15:28:44 -0500

    Update createTable.sql

--- createTable.sql
+++ createTable.sql
@@ -2,7 +2,7 @@ use iot;
 
 CREATE TABLE `stats` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `value` varchar(50) DEFAULT NULL,
+  `value` varchar(200) DEFAULT NULL,
   `type` varchar(50) DEFAULT NULL,
   `dateOfOccurance` datetime DEFAULT NULL,
   `nameOfMachine` varchar(50) DEFAULT NULL,
